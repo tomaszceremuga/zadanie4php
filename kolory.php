d7b342e2c8dffc842fa30f398857302e85888093
Tomasz Ceremuga <84967908+tomaszceremuga@users.noreply.github.com>
2023-10-04 12:19:44 +0200

    Update kolory.php

--- kolory.php
+++ kolory.php
@@ -9,18 +9,21 @@
 <?php
   $tlo = $_POST["tlo"];
   $tekst = $_POST["tekst"];
-  echo "<style>
-  p{
-    height: 50%;
-    width: 50%;
-    margin: 50px 50px 0 50px;
-    border: 2px dashed red;
-    background: {$tlo};
-    color: {$tekst};
-  }
-  </style>";
-  echo "<p>Tomasz Ceremuga</p>";
-
+  if (preg_match('/^[0-9A-Fa-f]+$/', $tlo) && preg_match('/^[0-9A-Fa-f]+$/', $tekst) ) {
+    echo "<style>
+    p{
+      height: 50%;
+      width: 50%;
+      margin: 50px 50px 0 50px;
+      border: 2px dashed red;
+      background: #{$tlo};
+      color: #{$tekst};
+    }
+    </style>";
+    echo "<p>Tomasz Ceremuga</p>";
+} else {
+    echo "Podaleś złe kolory";
+}
 ?>
 </body>
 </html>
